0f301eeefc6ca58a5537ae601907a66356cfb3e8
va1m <va1m@email.com>
2020-10-18 16:37:54 +0200

    Implement simple price providers for cold, hot, disposed water, and for electricity

---
+++ src/main/java/com/va1m/moskommunalbot/priceproviders/ElectricityPricesProvider.java
@@ -0,0 +1,18 @@
+package com.va1m.moskommunalbot.priceproviders;
+
+import java.time.LocalDate;
+
+/**
+ * Provides prices for electricity with durations during which the prices are valid
+ *
+ * https://www.mosenergosbyt.ru/individuals/tariffs-n-payments/tariffs-msk/kvartiry-i-doma-s-elektricheskimi-plitami.php
+ */
+public class ElectricityPricesProvider {
+
+    /** Provides prices with the durations they are being applying */
+    public PriceEntry[] provide() {
+        return new PriceEntry[] {
+                PriceEntry.of(LocalDate.of(2020, 7, 1), LocalDate.of(2020, 12, 31).minusDays(1), 487)
+        };
+    }
+}
---
+++ src/main/java/com/va1m/moskommunalbot/priceproviders/HotWaterPricesProvider.java
@@ -0,0 +1,22 @@
+package com.va1m.moskommunalbot.priceproviders;
+
+import java.time.LocalDate;
+
+/**
+ * Provides prices for cold water with durations during which the prices are valid
+ *
+ * https://online.moek.ru/clients/tarify-i-raschety/tarify
+ */
+public class HotWaterPricesProvider {
+
+    /** Provides prices with the durations they are being applying */
+    public PriceEntry[] provide() {
+        return new PriceEntry[] {
+                PriceEntry.of(LocalDate.of(2020, 1, 1), LocalDate.of(2020, 7, 1).minusDays(1), 19819),
+                PriceEntry.of(LocalDate.of(2020, 7, 1), LocalDate.of(2021, 7, 1).minusDays(1), 20515),
+                PriceEntry.of(LocalDate.of(2021, 7, 1), LocalDate.of(2022, 7, 1).minusDays(1), 20696),
+                PriceEntry.of(LocalDate.of(2022, 7, 1), LocalDate.of(2023, 7, 1).minusDays(1), 22285),
+                PriceEntry.of(LocalDate.of(2023, 7, 1), LocalDate.of(2024, 1, 1).minusDays(1), 22482)
+        };
+    }
+}
