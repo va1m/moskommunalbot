58a1bcb6bb1d5ee038311a360e26cb70757a3f09
va1m <va1m@email.com>
2020-12-30 21:35:39 +0100

    2021 tariffs

--- src/test/java/com/va1m/testools/Json.java
+++ src/test/java/com/va1m/testools/Json.java
@@ -1,5 +1,7 @@
 package com.va1m.testools;
 
+import static java.util.Objects.requireNonNull;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
@@ -16,6 +18,7 @@ public final class Json {
     /** Deserializes a json file to a given type object */
     public static <T> T deserialize(String path, Class<T> type) {
         try (InputStream is = Json.class.getClassLoader().getResourceAsStream(path)) {
+            requireNonNull(is, () -> "File '" + path + "' is not found");
             final var content = new String(is.readAllBytes());
             return OBJECT_MAPPER.readValue(content, type);
         } catch (IOException e) {
